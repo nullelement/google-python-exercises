2b54691535471abeb7594ec834c31de113267fc2
nullelement <nullelement-dev@yahoo.com.br>
2016-08-22 17:29:01 -0300

    basic 1

--- basic/list1.py
+++ basic/list1.py
@@ -23,7 +23,11 @@
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
     # +++your code here+++
-    return
+    n = 0
+    for word in words:
+        if len(word) >= 2 and word[0] == word[-1]:
+            n += 1
+    return n
 
 
 # B. front_x
